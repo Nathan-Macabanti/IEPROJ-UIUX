dac04f1ed3a7544e4c3f9c72bf1d86a22deb2d91
hygrintoht <francis_santonia@dlsu.edu.ph>
2021-11-14 14:14:22 +0800

    Created a template for the main menu

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -9,4 +9,8 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Prototype");
     }
+    public void onPlayerExit()
+    {
+        Application.Quit();
+    }
 }
---
+++ Assets/Script/PlayerPrefsManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this script manages all the shop and item interactions in the game
+//data must be pre-loaded at the start of the scene other wise a lag spike will occure
+
+public class PlayerPrefsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
