c0e24f86fd563fae21fa6571c9d924ba96cb22f1
Khalil Jan Arcilla <khalil_jan_arcilla@dlsu.edu.ph>
2022-02-08 05:47:28 +0800

    stuff

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+	private bool SpacePress = false;
+	public Image image;
+	public GameObject title;
+
+	void Start()
+	{
+		image = title.GetComponent<Image>();
+	}
     public void onPlayPress()
     {
-        SceneManager.LoadScene("ChapterSelection");
+        SceneManager.LoadScene("tutorial_1.0");
     }
 
     public void onPlayGamePress()
     {
         SceneManager.LoadScene("Prototype");
     }
+    void Update()
+    {
+	    if (Input.GetKeyDown(KeyCode.Space) && SpacePress == false)
+	    {
+			
+		    SpacePress = true;
+		    image.CrossFadeColor(Color.black, 5.0f, true, true);
+		    StartCoroutine(deactivate(5.0f));
+	    }
+    }
 
+    public IEnumerator deactivate(float t)
+    {
+	    yield return new WaitForSeconds(t);
+		title.SetActive(false);
+    }
 }
