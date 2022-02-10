85b45abc36a184c99d2be70bf8b679885b6a4561
Nathan-Macabanti <73865062+Nathan-Macabanti@users.noreply.github.com>
2022-02-10 23:20:14 +0800

    Fixed spawning

--- Assets/Script/Tutorial/TutorialManager.cs
+++ Assets/Script/Tutorial/TutorialManager.cs
@@ -22,6 +22,11 @@ public class TutorialManager : MonoBehaviour
 
     //jump spawner
 
+    [SerializeField] private float _bpm;
+    [SerializeField] private float _beatInterval, _beatTimer;
+    [SerializeField] private static bool _beatFull;
+    [SerializeField] private static int _beatCountFull;
+    [SerializeField] private bool SpawningAllowed;
 
     public float waitTime = 5f;
     private float startTime;
@@ -38,11 +43,24 @@ public class TutorialManager : MonoBehaviour
     void Start()
     {
         dialogueT = dialogueManager.GetComponent<Dialogue>();
+        SpawningAllowed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        _beatFull = false;
+        _beatInterval = 60 / _bpm;
+        _beatTimer += Time.deltaTime;
+
+        if (_beatTimer >= _beatInterval && !SpawningAllowed)
+        {
+            _beatTimer -= _beatInterval;
+            _beatFull = true;
+            _beatCountFull++;
+            Debug.Log("Full");
+        }
+
         Debug.Log("Current PopUp no.: " + popUpIndex);
 
         for(int i = 0; i<popUps.Length; i++)
@@ -58,70 +76,68 @@ public class TutorialManager : MonoBehaviour
 
         }
 
-
-            if(popUpIndex == 0)
+        if(popUpIndex == 0)
+        {
+            //popUps[0].SetActive(false);
+            dialogueObj.SetActive(true);
+            if(dialogueT.dialogueEnded == true)
             {
-                //popUps[0].SetActive(false);
-                dialogueObj.SetActive(true);
-                if(dialogueT.dialogueEnded == true)
+                Debug.Log("Dialogue ended!");
+                dialogueObj.SetActive(false);
+                popUpIndex++;
+            }
+
+        }
+        else if (popUpIndex == 1)
+        {
+            
+            SpawningAllowed = true;
+            BeatDetection(0,3);
+                    
+            // Movement
+            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+            {
+                if (movementCount <= moveCriteria)
                 {
-                    Debug.Log("Dialogue ended!");
-                    dialogueObj.SetActive(false);
-                    popUpIndex++;
+                    movementCount++;
                 }
-
-            }
-            else if (popUpIndex == 1)
-            {
-                    leftSpawner.SpawnDodgeNote();
-                    midSpawner.SpawnDodgeNote();
-                    rightSpawner.SpawnDodgeNote();
-                    // Movement
-                    if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
-                    {
-                        if (movementCount <= moveCriteria)
-                        {
-                            movementCount++;
-                        }
-                        else
-                        {
-                            leftSpawner.DestroyAllNotes();
-                            rightSpawner.DestroyAllNotes();
-                            midSpawner.DestroyAllNotes();
-                            popUpIndex++;
-                        }
-                    }
-
-            }
-            else if (popUpIndex == 2)
-            {
-
-                    midSpawner.SpawnJumpNote();
-                // Jumping
-                if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+                else
                 {
-                    if (jumpCount <= jumpCriteria)
-                    {
-                        jumpCount++;
-                    }
-                    else
-                    {
+                    leftSpawner.DestroyAllNotes();
+                    rightSpawner.DestroyAllNotes();
                     midSpawner.DestroyAllNotes();
                     popUpIndex++;
-                    }
-
+                }
+            }
+            Debug.Log("Starting Moving tutorial");
+        }
+        else if (popUpIndex == 2)
+        {
+            
+            SpawningAllowed = true;
+            BeatDetection(3,4);
+            // Jumping
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            {
+                if (jumpCount <= jumpCriteria)
+                {
+                    jumpCount++;
+                }
+                else
+                {
+                midSpawner.DestroyAllNotes();
+                popUpIndex++;
                 }
 
-
             }
-            else if (popUpIndex == 3)
-            {
+            Debug.Log("Starting jumping tutorial");
+        }
+        else if (popUpIndex == 3)
+        {
 
-                StartCoroutine(TransitiontoFirstLevel(waitTime));
-                popUpIndex++;
-
-
-            }
+            StartCoroutine(TransitiontoFirstLevel(waitTime));
+            popUpIndex++;
+        }
 
 
        IEnumerator TransitiontoFirstLevel(float t)
@@ -133,7 +149,48 @@ public class TutorialManager : MonoBehaviour
 
     }
 
-   
+   void BeatDetection(int first, int last)
+    {
+        
+        if (_beatTimer >= _beatInterval)
+        {
+            int SpawnIndex = Random.Range(first, last);
+            _beatTimer -= _beatInterval;
+            _beatFull = true;
+            _beatCountFull++;
+            Debug.Log("Full");
+            if (SpawningAllowed)
+            {
+                switch (SpawnIndex)
+                {
+                    case 0: SpawnLeft(); break;
+                    case 1: SpawnMid(); break;
+                    case 2: SpawnRight(); break;
+                    case 3: SpawnJump(); break;
+                    default: Debug.Log("Out of index"); break;
+                }
+            }
+        }
 
+    }
 
+    void SpawnLeft()
+    {
+        leftSpawner.SpawnDodgeNote();
+    }
+
+    void SpawnMid()
+    {
+        midSpawner.SpawnDodgeNote();
+    }
+
+    void SpawnRight()
+    {
+        rightSpawner.SpawnDodgeNote();
+    }
+
+    void SpawnJump()
+    {
+        midSpawner.SpawnJumpNote();
+    }
 }
